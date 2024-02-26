dd8f39b8d100329d68330db9d958ca8074ef8def
Menatalla Gomaa <minafathi98@gmail.com>
2024-02-26 14:42:47 +0100

    fixing a typo

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,11 +1,11 @@
 import useMediaQuery from "@/hooks/useMediaQurery";
 import { SelectedPage } from "@/shared/types";
 import ActionButton from "@/shared/ActionButton";
-import HomePageText from "@/assets/HomePageText.png";
-import HomePageGraphic from "@/assets/HomePageGraphic.png";
-import SponsorRedBull from "@/assets/SponsorRedBull.png";
-import SponsorForbes from "@/assets/SponsorForbes.png";
-import SponsorFortune from "@/assets/SponsorFortune.png";
+import HomePageText from "./assets/HomePageText.png";
+import HomePageGraphic from "./assets/HomePageText.png";
+import SponsorRedBull from "./assets/SponsorRedBull.png";
+import SponsorForbes from "./assets/SponsorForbes.png";
+import SponsorFortune from "./assets/SponsorFortune.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import { motion } from "framer-motion";
 
