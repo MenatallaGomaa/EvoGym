65914e96d4b5d3ddd6ebd6360efb8295ca45b353
Menatalla Gomaa <minafathi98@gmail.com>
2024-02-29 13:35:28 +0200

    updating the prettier.config file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Navbar from "@/scenes/navbar";
-import Home from "./scenes/home";
+import Home from "@/scenes/home";
+import OurClasses from "@/scenes/ourClasses";
+import Benefits from "@/scenes/benefits";
+import ContactUs from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
 import { useEffect, useState } from "react";
-import { SelectedPage } from "./shared/types";
+import { SelectedPage } from "@/shared/types";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -28,6 +32,11 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
